1e94d8f409b30d84a2986dc1d29278b98dac46a3
Nader-Eryan <ne1309@fayoum.edu.eg>
2023-11-14 03:24:16 +0200

    first commit

---
+++ lib/Features/home/presentation/view/home_view.dart
@@ -0,0 +1,25 @@
+import 'package:dating/core/utils/styles.dart';
+import 'package:flutter/material.dart';
+
+class HomeView extends StatelessWidget {
+  const HomeView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: SafeArea(
+          child: Column(
+        children: [
+          Text(
+            'This is the test bold text',
+            style: Styles.kH3,
+          ),
+          Text(
+            'This is the test regular text',
+            style: TextStyle(fontSize: 24),
+          ),
+        ],
+      )),
+    );
+  }
+}
---
+++ lib/core/utils/styles.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+abstract class Styles {
+  static const kH1Bold = TextStyle(fontSize: 34, fontWeight: FontWeight.bold);
+
+  static const kH3 = TextStyle(fontSize: 24);
+  static const kH3Bold = TextStyle(fontSize: 24, fontWeight: FontWeight.bold);
+
+  static const kH4 = TextStyle(fontSize: 18);
+  static const kH4Bold = TextStyle(fontSize: 18, fontWeight: FontWeight.bold);
+
+  static const kH5 = TextStyle(fontSize: 16);
+  static const kH5Bold = TextStyle(fontSize: 16, fontWeight: FontWeight.bold);
+
+  static const kH6 = TextStyle(fontSize: 14);
+  static const kH6Bold = TextStyle(fontSize: 14, fontWeight: FontWeight.bold);
+
+  static const kH7 = TextStyle(fontSize: 12);
+  static const kH7Bold = TextStyle(fontSize: 12, fontWeight: FontWeight.bold);
+}

ffbb8236e31e95e5c360356ea70c10d236df4448
Nader-Eryan <ne1309@fayoum.edu.eg>
2023-11-25 18:49:22 +0200

    updated gradle versions

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,15 @@
 import 'package:dating/core/utils/go_router.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
   runApp(const MyApp());
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 }
 
 class MyApp extends StatelessWidget {
